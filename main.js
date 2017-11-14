d19a3f185791704b6142ec1aef2d341ff06c89f3
MrElektronix <milovanpelt@gmail.com>
2017-11-14 23:13:58 +0100

    Setup

---
+++ main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+	
+function preload() {
+}
+
+function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+}
+
+function update() {
+}
